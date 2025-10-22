62d9ce73c3c6eac165c1ce3ca50ea1efd426c6f1
Adoba Yua <adoba@alpharora.com>
2025-10-21 22:55:13 -0500

    sturn

--- components/sections/use-cases-section.tsx
+++ components/sections/use-cases-section.tsx
@@ -99,7 +99,7 @@ export function UseCasesSection() {
   return (
     <section
       ref={ref}
-      className="flex h-screen w-screen shrink-0 snap-start items-center px-6 pt-20 md:px-12 md:pt-0 lg:px-16"
+      className="flex h-screen w-screen shrink-0 snap-start items-center px-6 py-24 md:px-12 lg:px-16"
     >
       <div className="mx-auto w-full max-w-7xl">
         <div
